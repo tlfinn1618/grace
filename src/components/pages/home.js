6ea90353602218ddcb00710c3dd266e7b161b8af
Thomas Finn <tlfinn1618@gmail.com>
2022-10-18 15:56:49 +0000

    Update 3 files. Added some content to home component with adding a home.scss style sheet

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -2,8 +2,24 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className="main-container">
+      <div className="main-content">
+        <div className="main-content-left">
+          <div className="main-content-heading">Who are we?</div>
+          <div className="main-content-text">
+            We are your neighbors who want only to make sure that you do not
+            miss the grace of God. (Hebrews 12:25) Join us to hear it,
+            experience it, and learn to give it to others. On your first visit
+            you’ll find a Jesus centered, grace oriented Adventism striving to
+            love each other within a racially, generationally, & culturally
+            diverse community of believers. Pastor Greg Webster
+          </div>
+        </div>
+        <div className="main-content-right">
+          <div className="main-content-worship">Worship times</div>
+          <div className="main-content-prayers">Prayers List component</div>
+        </div>
+      </div>
     </div>
   );
 }

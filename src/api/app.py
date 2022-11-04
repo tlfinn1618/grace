b1492e88d2ddadaae31fcf7dbc84cc3c8eaa048f
tlfinn1618 <tlfinn1618@gmail.com>
2022-11-03 19:53:08 -0700

    restarted the api...again. Just can't get happy with it.

--- src/api/app.py
+++ src/api/app.py
@@ -1,36 +1,11 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-import os
-
 
 app = Flask(__name__)
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(basedir, 'gracebase.sqlite')
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(50), unique=True)
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('username', 'password')
-
-
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+@app.route('/')
+def hello():
+    return "Hey Tom"
 
 
 if __name__ == '__main__':

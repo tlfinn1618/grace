f7dde022164922c059f3bcaec6d121f8cb42f6a9
tlfinn1618 <tlfinn1618@gmail.com>
2022-12-02 13:03:11 -0700

    updated pip and now have a working api connected to gracebase.

--- src/grace_api/churches.py
+++ src/grace_api/churches.py
@@ -2,4 +2,62 @@
 
 from flask import abort, make_response
 from config import db
-from models import Church, people_schema, person_schema
+from models import Church, church_schema, churches_schema
+
+def read_all():
+    churches = Church.query.all()
+    return churches_schema.dump(churches)
+
+def create(church):
+    name = church.get("name")
+    existing_church = Church.query.filter(Church.name == name).one_or_none()
+    
+    if existing_church is None:
+        new_church = church_schema.load(church, session=db.session)
+        db.session.add(new_church)
+        db.session.commit()
+        return church_schema.dump(new_church), 201
+    else:
+        abort(
+            406,
+            f"{name} already exists in the list"
+        )
+        
+def read_one(name):
+    name = Church.query.filter(Church.name == name).one_or_none()
+    
+    if name is not None:
+        return church_schema.dump(name)
+    else:
+        abort(
+            404, f"{name} is not found"
+        )
+        
+def update(name, church):
+    existing_church = Church.query.filter(Church.name == name).one_or_none()
+    
+    if existing_church:
+        update_church = church_schema.load(church, session=db.session)
+        existing_church.name = update_church.name
+        db.session.merge(existing_church)
+        db.session.commit()
+        return church_schema.dump(existing_church), 201
+    else:
+        abort(
+            404,
+            f"User with last name {name} is not found"
+        )
+        
+def delete(name):
+    existing_church = Church.query.filter(Church.name == name).one_or_none()
+    
+    if existing_church:
+        db.session.delete(existing_church)
+        db.session.commit()
+        return make_response(f"{name} successfully deleted", 200)
+    else:
+        abort(
+            404, 
+            f"The church with the name of {name} is not found"
+        )
+        
--- src/grace_api/models.py
+++ src/grace_api/models.py
@@ -1,13 +1,13 @@
 
 
 from datetime import datetime
-from config import db 
+from config import db, ma
 
 class Church(db.Model):
     __tablename__ = "church"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
-    address1 = db.Column(db.String)
+    address = db.Column(db.String)
     address2 = db.Column(db.String)
     city = db.Column(db.String)
     state = db.Column(db.String)
@@ -15,4 +15,12 @@ class Church(db.Model):
     phone = db.Column(db.String)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     
+class ChurchSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Church
+        load_instance = True
+        sqla_session = db.session
+        
+church_schema = ChurchSchema()
+churches_schema = ChurchSchema(many=True)
     
